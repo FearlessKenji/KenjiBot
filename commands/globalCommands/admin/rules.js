abdec344857d11b802cced1bdc7d34c73552cc30
FearlessKenji <160550099+FearlessKenji@users.noreply.github.com>
2026-06-16 17:40:15 -0400

    Expand default server rules text

--- commands/globalCommands/admin/rules.js
+++ commands/globalCommands/admin/rules.js
@@ -22,7 +22,15 @@ const RULES_TITLE_INPUT_ID = `title`;
 const RULES_BODY_INPUT_ID = `body`;
 const RULES_VERIFICATION_COLOR = 0x57f287;
 const DEFAULT_RULES_TITLE = `Server Rules`;
-const DEFAULT_RULES_BODY = `Please read and understand the rules before participating.`;
+const DEFAULT_RULES_BODY = `Please read and understand the following:
+
+1. Treat everyone with respect. Absolutely no harassment, witch hunting, sexism, racism, or hate speech will be tolerated.
+
+2. No spam or self-promotion (server invites, advertisements, etc) without permission from a staff member. This includes DMing fellow members.
+
+3. No NSFW or obscene content. This includes text, images, or links featuring nudity, sex, hard violence, or other graphically disturbing content.
+
+4. If you see something against the rules or something that makes you feel unsafe, let staff know. We want this server to be a welcoming space!`;
 const RULES_VERIFICATION_DESCRIPTION = `Once you have read and understand the rules, react with ${RULES_VERIFICATION_EMOJI} to gain access to the rest of the server.`;
 
 function hasPermission(interaction, permission) {
